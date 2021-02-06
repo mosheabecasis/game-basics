f3aef9b7ace1cf2a119c0015cd7a9333559b282e
moshe abecasis <mosh14abou@yahoo.com>
2021-02-06 15:45:54 +0200

    alfa

--- main.js
+++ main.js
@@ -3,79 +3,20 @@ const $ball = document.getElementById('ball');
 const $body = document.getElementById('body');
 const $button = document.getElementById('button');
 
-//let x = $ball.style.left;
-//let y = $ball.style.top;
+ 
 let radius = 50;
 let speed = 10;
-let gameTime;
+//let gameTime;
 
-let up = false;
-let down = false;
-let left = false;
-let right = false;
+$button.addEventListener('click', () => {
+    $ball.style.position = 'absolute';
+    $ball.style.left = 0;
+    $ball.style.top = 0;
+});
 
-const init = ()=>{
-   gameTime = setTimeout(status, 1000);
-  //status();
-   //checkLimit(); 
-}
-
-const status = ()=>{
-    if (up) {
-        $ball.style.top -= speed;
-    }
-    if (down) {
-        $ball.style.top += speed;
-    }
-    if (left) {
-        $ball.style.left -= speed;
-    }
-    if (right) {
-        $ball.style.left += speed;
-    } 
-}
-
-const keyDown = (event)=>{
-    console.log(event);
-    //up
-    if (event.keyCode == 38) {
-        up = true;
-    }
-
-    //down
-    if (event.keyCode == 40) {
-        down = true;
-    }
-    //left
-    if (event.keyCode == 37) {
-        left = true;
-    }
-
-    //right
-    if (event.keyCode == 39) {
-        right = true;
-    }
-}
-const keyUp = (event)=>{
-    //up
-    if (event.keyCode == 38) {
-        up = false;
-    }
-
-    //down
-    if (event.keyCode == 40) {
-        down = false;
-    }
-    //left
-    if (event.keyCode == 37) {
-        left = false;
-    }
-
-    //right
-    if (event.keyCode == 39) {
-        right = false;
-    }
-}
-$button.addEventListener('click', init);
-$body.addEventListener("keydown", keyDown);
-$body.addEventListener("keyup", keyUp);
+$body.addEventListener('keydown', ($event) => {
+    if ($event.key == 'ArrowLeft' ) {$ball.style.left = parseInt($ball.style.left) - speed + 'px';}
+    if ($event.key == 'ArrowRight') {$ball.style.left = parseInt($ball.style.left) + speed + 'px'; }    
+    if ($event.key == 'ArrowUp') {$ball.style.top = parseInt($ball.style.top) - speed + 'px'; }  
+    if ($event.key == 'ArrowDown') {$ball.style.top = parseInt($ball.style.top) + speed + 'px'; }  
+});
