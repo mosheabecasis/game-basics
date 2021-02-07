73982cccfb1813f82e09c1213f8291ad4c81b971
moshe abecasis <mosh14abou@yahoo.com>
2021-02-07 13:29:28 +0200

    alfa8

--- main.js
+++ main.js
@@ -11,17 +11,22 @@ let cuurentPositionX = 0;
 
 $button.addEventListener('click', () => {
     $ball.style.position = 'absolute';
-    $ball.style.left = 0;
-    $ball.style.top = 0;
+    $ball.style.left = 170 +'px';
+    $ball.style.top = 170 +'px';
     //setInterval(borderCheck, 500);
     $button.innerHTML = 'reset';
+    $ball.style.backgroundColor = 'white';
 });
 
 $body.addEventListener('keydown', ($event) => {
-    if ($event.key == 'ArrowLeft' ) {$ball.style.left = parseInt($ball.style.left) - speed + 'px';}
-    if ($event.key == 'ArrowRight') {$ball.style.left = parseInt($ball.style.left) + speed + 'px'; }  
-    if ($event.key == 'ArrowUp') {$ball.style.top = parseInt($ball.style.top) - speed + 'px'; }  
-    if ($event.key == 'ArrowDown') {$ball.style.top = parseInt($ball.style.top) + speed + 'px'; } 
+    if ($event.key == 'ArrowLeft' ) {$ball.style.left = parseInt($ball.style.left) - speed + 'px';
+                                     $ball.style.backgroundColor = 'red';}
+    if ($event.key == 'ArrowRight') {$ball.style.left = parseInt($ball.style.left) + speed + 'px';
+                                     $ball.style.backgroundColor = 'blue'; }  
+    if ($event.key == 'ArrowUp') {$ball.style.top = parseInt($ball.style.top) - speed + 'px';
+                                  $ball.style.backgroundColor = 'green'; }  
+    if ($event.key == 'ArrowDown') {$ball.style.top = parseInt($ball.style.top) + speed + 'px';
+                                    $ball.style.backgroundColor = 'yellow'; } 
      cuurentPositionY = parseInt($ball.style.top);
      cuurentPositionX = parseInt($ball.style.left);
     borderCheck(); 
